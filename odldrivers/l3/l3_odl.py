4c4669c1c388a977010882d7731734c5ae668f83
Dave Tucker <dave@dtucker.co.uk>
2014-08-13 23:57:54 +0100

    Revert "Add callbacks for router interface add/remove"
    Looks like we get most of this via the port update.
    Therefore I don't think this is necessary right now.
    
    This reverts commit e1257dd079d963be39e7b77de9a66a1ec9dcb0a4.

--- odldrivers/l3/l3_odl.py
+++ odldrivers/l3/l3_odl.py
@@ -30,8 +30,6 @@ from odldrivers.common import utils
 
 ROUTERS = 'routers'
 FLOATINGIPS = 'floatingips'
-ADD_ROUTER_INTERFACE = 'add_router_interface'
-REMOVE_ROUTER_INTERFACE = 'remove_router_interface'
 
 
 class L3RouterPluginRpcCallbacks(n_rpc.RpcCallback,
@@ -116,20 +114,8 @@ class OpenDaylightL3RouterPlugin(common_db_mixin.CommonDbMixin,
         url = ROUTERS + "/" + id
         self.client.sendjson('delete', url, None)
 
-    def add_router_interface(self, context, router_id, interface_info):
-        info = super(OpenDaylightL3RouterPlugin, self).add_router_interface(
-            context, router_id, interface_info)
-        url = ROUTERS + "/" + router_id + "/" + ADD_ROUTER_INTERFACE
-        self.client.sendjson('put', url, info, None)
-        return info
-
-    def remove_router_interface(self, context, router_id, interface_info):
-        info = super(OpenDaylightL3RouterPlugin, self).remove_router_interface(
-            context, router_id, interface_info)
-        url = ROUTERS + "/" + router_id + "/" + REMOVE_ROUTER_INTERFACE
-        self.client.sendjson('put', url, info, None)
-        return info
-
+    #def add_router_interface(self, context, router_id, interface_info):
+    #def remove_router_interface(self, context, router_id, interface_info):
     def create_floatingip(self, context, floatingip,
                           initial_status=q_const.FLOATINGIP_STATUS_ACTIVE):
         fip_dict = super(OpenDaylightL3RouterPlugin, self).create_floatingip(
