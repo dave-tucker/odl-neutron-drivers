8060c855f28926aa0d4c39daf79edb97496143cb
Dave Tucker <dave@dtucker.co.uk>
2014-09-04 23:34:41 +0100

    Install OVS 2.1.2 by default

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,12 +1,97 @@
-package {"git":
-    ensure => "installed"
-}
+$deps = [ 'build-essential',
+          'debhelper',
+          'dkms',
+          'fakeroot',
+          'graphviz',
+          'git',
+          'linux-headers-generic',
+          'python-all',
+          'python-qt4',
+          'python-zopeinterface',
+          'python-twisted-conch',
+          'python-twisted-web',
+          'xauth'
+]
 
 $hosts = hiera('hosts')
 
-file { "/etc/hosts":
-    ensure => file,
-    owner => "root",
-    group => "root",
+file { '/etc/hosts':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
     content => template('/vagrant/puppet/templates/hosts.erb')
+}
+
+package { $deps:
+    ensure   => installed,
+}
+
+vcsrepo { '/home/vagrant/mininet':
+    ensure   => present,
+    provider => git,
+    user     => 'vagrant',
+    source   => 'git://github.com/mininet/mininet',
+    revision => '2.1.0p2',
+    before   => Exec['Install Mininet']
+}
+
+exec { 'Install Mininet':
+    command => 'bash mininet/util/install.sh -nf > /dev/null',
+    cwd     => '/home/vagrant',
+    user    => 'vagrant',
+    path    => $::path,
+    timeout => 0
+}
+
+exec {'openvswitch-2.1.2.tar.gz':
+    command => 'wget http://openvswitch.org/releases/openvswitch-2.1.2.tar.gz',
+    cwd     => '/home/vagrant',
+    path    => $::path,
+    user    => 'vagrant'
+}
+
+exec { 'Extract Open vSwitch':
+    command => 'tar -xvf openvswitch-2.1.2.tar.gz',
+    cwd     => '/home/vagrant',
+    user    => 'vagrant',
+    path    => $::path,
+    timeout => 0,
+    require => Exec['openvswitch-2.1.2.tar.gz']
+}
+
+exec { 'Compile Open vSwitch':
+    command => 'fakeroot debian/rules binary',
+    cwd     => '/home/vagrant/openvswitch-2.1.2',
+    user    => 'root',
+    path    => $::path,
+    timeout => 0,
+    require => [Exec['Extract Open vSwitch'], Package[$deps]]
+}
+
+package { 'openvswitch-common':
+    ensure   => installed,
+    provider => dpkg,
+    source   => '/home/vagrant/openvswitch-common_2.1.2-1_amd64.deb',
+    require  => Exec['Compile Open vSwitch']
+}
+
+package { 'openvswitch-switch':
+    ensure   => installed,
+    provider => dpkg,
+    source   => '/home/vagrant/openvswitch-switch_2.1.2-1_amd64.deb',
+    require  => Package['openvswitch-common']
+}
+
+package { 'openvswitch-datapath-dkms':
+    ensure   => installed,
+    provider => dpkg,
+    source   => '/home/vagrant/openvswitch-datapath-dkms_2.1.2-1_all.deb',
+    require  => Package['openvswitch-switch']
+}
+
+package { 'openvswitch-pki':
+    ensure   => installed,
+    provider => dpkg,
+    source   => '/home/vagrant/openvswitch-pki_2.1.2-1_all.deb',
+    require  => Package['openvswitch-datapath-dkms']
 }
