02e3b528f8a07b1298b5d13955c1dc181c559324
Dave Tucker <dave@dtucker.co.uk>
2014-09-05 01:19:05 +0100

    Add Devstack Pacakges and Install Drivers By Default

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,19 +1,110 @@
-$deps = [ 'build-essential',
-          'debhelper',
-          'dkms',
-          'fakeroot',
-          'graphviz',
-          'git',
-          'linux-headers-generic',
-          'python-all',
-          'python-qt4',
-          'python-zopeinterface',
-          'python-twisted-conch',
-          'python-twisted-web',
-          'xauth'
+$deps = [
+    'bc',
+    'bridge-utils',
+    'build-essential',
+    'conntrack',
+    'curl',
+    'debhelper',
+    'dkms',
+    'dnsmasq-base',
+    'dnsmasq-utils',
+    'ebtables',
+    'euca2ools',
+    'fakeroot',
+    'gawk',
+    'gcc',
+    'genisoimage',
+    'git',
+    'graphviz',
+    'iptables',
+    'iputils-arping',
+    'iputils-ping',
+    'kpartx',
+    'libffi-dev',
+    'libjs-jquery-tablesorter',
+    'libkrb5-dev',
+    'libldap2-dev',
+    'libmysqlclient-dev',
+    'libpq-dev',
+    'libsasl2-dev',
+    'libssl-dev',
+    'libvirt-bin',
+    'libxml2-dev',
+    'libxslt1-dev',
+    'libyaml-dev',
+    'linux-headers-generic',
+    'lsof',
+    'mysql-server',
+    'openssh-server',
+    'openssl',
+    'parted',
+    'pm-utils',
+    'psmisc',
+    'pylint',
+    'python-all',
+    'python-boto',
+    'python-cheetah',
+    'python-cmd2',
+    'python-dev',
+    'python-eventlet',
+    'python-feedparser',
+    'python-greenlet',
+    'python-iso8601',
+    'python-kombu',
+    'python-libvirt',
+    'python-libxml2',
+    'python-lockfile',
+    'python-lxml',
+    'python-m2crypto',
+    'python-migrate',
+    'python-mock',
+    'python-mox',
+    'python-mysql.connector',
+    'python-mysqldb',
+    'python-numpy',
+    'python-paste',
+    'python-pastedeploy',
+    'python-pastescript',
+    'python-pip',
+    'python-pysqlite2',
+    'python-pyudev',
+    'python-qpid',
+    'python-qt4',
+    'python-routes',
+    'python-setuptools',
+    'python-sqlalchemy',
+    'python-suds',
+    'python-tempita',
+    'python-twisted-conch',
+    'python-twisted-web',
+    'python-unittest2',
+    'python-virtualenv',
+    'python-webob',
+    'python-wsgiref',
+    'python-xattr',
+    'python-zopeinterface',
+    'python2.7',
+    'qemu',
+    'qemu-kvm',
+    'qpidd',
+    'rabbitmq-server',
+    'radvd',
+    'screen',
+    'socat',
+    'sqlite3',
+    'sudo',
+    'tar',
+    'tcpdump',
+    'unzip',
+    'vlan',
+    'wget',
+    'xauth',
+    'zlib1g-dev'
 ]
 
 $hosts = hiera('hosts')
+
+$ovs_version = '2.3.0'
 
 file { '/etc/hosts':
     ensure  => file,
@@ -26,42 +117,25 @@ package { $deps:
     ensure   => installed,
 }
 
-vcsrepo { '/home/vagrant/mininet':
-    ensure   => present,
-    provider => git,
-    user     => 'vagrant',
-    source   => 'git://github.com/mininet/mininet',
-    revision => '2.1.0p2',
-    before   => Exec['Install Mininet']
-}
-
-exec { 'Install Mininet':
-    command => 'bash mininet/util/install.sh -nf > /dev/null',
-    cwd     => '/home/vagrant',
-    user    => 'vagrant',
-    path    => $::path,
-    timeout => 0
-}
-
-exec {'openvswitch-2.1.2.tar.gz':
-    command => 'wget http://openvswitch.org/releases/openvswitch-2.1.2.tar.gz',
+exec {"openvswitch-${ovs_version}.tar.gz":
+    command => "wget http://openvswitch.org/releases/openvswitch-${ovs_version}.tar.gz",
     cwd     => '/home/vagrant',
     path    => $::path,
     user    => 'vagrant'
 }
 
 exec { 'Extract Open vSwitch':
-    command => 'tar -xvf openvswitch-2.1.2.tar.gz',
+    command => "tar -xvf openvswitch-${ovs_version}.tar.gz",
     cwd     => '/home/vagrant',
     user    => 'vagrant',
     path    => $::path,
     timeout => 0,
-    require => Exec['openvswitch-2.1.2.tar.gz']
+    require => Exec["openvswitch-${ovs_version}.tar.gz"]
 }
 
 exec { 'Compile Open vSwitch':
     command => 'fakeroot debian/rules binary',
-    cwd     => '/home/vagrant/openvswitch-2.1.2',
+    cwd     => "/home/vagrant/openvswitch-${ovs_version}",
     user    => 'root',
     path    => $::path,
     timeout => 0,
@@ -71,27 +145,27 @@ exec { 'Compile Open vSwitch':
 package { 'openvswitch-common':
     ensure   => installed,
     provider => dpkg,
-    source   => '/home/vagrant/openvswitch-common_2.1.2-1_amd64.deb',
+    source   => "/home/vagrant/openvswitch-common_${ovs_version}-1_amd64.deb",
     require  => Exec['Compile Open vSwitch']
 }
 
 package { 'openvswitch-switch':
     ensure   => installed,
     provider => dpkg,
-    source   => '/home/vagrant/openvswitch-switch_2.1.2-1_amd64.deb',
+    source   => "/home/vagrant/openvswitch-switch_${ovs_version}-1_amd64.deb",
     require  => Package['openvswitch-common']
 }
 
 package { 'openvswitch-datapath-dkms':
     ensure   => installed,
     provider => dpkg,
-    source   => '/home/vagrant/openvswitch-datapath-dkms_2.1.2-1_all.deb',
+    source   => "/home/vagrant/openvswitch-datapath-dkms_${ovs_version}-1_all.deb",
     require  => Package['openvswitch-switch']
 }
 
 package { 'openvswitch-pki':
     ensure   => installed,
     provider => dpkg,
-    source   => '/home/vagrant/openvswitch-pki_2.1.2-1_all.deb',
+    source   => "/home/vagrant/openvswitch-pki_${ovs_version}-1_all.deb",
     require  => Package['openvswitch-datapath-dkms']
 }
--- puppet/manifests/devstack-compute.pp
+++ puppet/manifests/devstack-compute.pp
@@ -1,16 +1,23 @@
-vcsrepo { "/home/vagrant/devstack":
+vcsrepo {'/home/vagrant/devstack':
+    ensure   => present,
     provider => git,
-    ensure => present,
-    user => "vagrant",
-    source => "https://github.com/openstack-dev/devstack.git",
-    before => File['/home/vagrant/devstack/local.conf']
+    user     => 'vagrant',
+    source   => 'https://github.com/openstack-dev/devstack.git',
+    before   => File['/home/vagrant/devstack/local.conf']
 }
 
 $hosts = hiera('hosts')
 
-file { "/home/vagrant/devstack/local.conf":
-    ensure => present,
-    owner => "vagrant",
-    group => "vagrant",
+file { '/home/vagrant/devstack/local.conf':
+    ensure  => present,
+    owner   => 'vagrant',
+    group   => 'vagrant',
     content => template('/vagrant/puppet/templates/compute.local.conf.erb')
+}
+
+exec { 'Install Drivers':
+    command => 'pip install -r requirements.txt && python setup.py install',
+    cwd     => '/vagrant',
+    path    => $::path,
+    user    => 'root'
 }
